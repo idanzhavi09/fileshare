92a8abcdf78131cc116009e881d076fa37458b37
Idan Zehavi <idanzhavi09@gmail.com>
2022-05-04 11:48:52 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
@@ -9,9 +12,9 @@ class fileSharePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(248, 233, 213, 100),
+      backgroundColor: Color.fromARGB(255, 219, 180, 173),
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        backgroundColor: Color.fromARGB(255, 211, 12, 123),
         title: Center(child: Text("FileShare")),
       ),
       body: share(),
@@ -25,14 +28,84 @@ class share extends StatefulWidget {
 }
 
 class _shareState extends State<share> {
+  var passcode = 'Passcode';
+  Duration duration = Duration();
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    startTimer();
+  }
+
+  void addTime() {
+    final addSeconds = 1;
+
+    setState(() {
+      final seconds = duration.inSeconds + addSeconds;
+
+      duration = Duration(seconds: seconds);
+    });
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) => {addTime()});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: TextButton(
-      child: Image.asset('images/dropfilehere.png'),
-      onPressed: () async {
-        final result = await FilePicker.platform.pickFiles(allowMultiple: true);
-      },
-    ));
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final minuts = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildTimeCard(time: minuts, header: 'Minuts'),
+            const SizedBox(width: 8),
+            buildTimeCard(time: seconds, header: 'Seconds'),
+          ],
+        ),
+        Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(passcode),
+            TextButton(
+              child: Image.asset('images/dropfilehere.png'),
+              onPressed: () async {
+                final result =
+                    await FilePicker.platform.pickFiles(allowMultiple: true);
+              },
+            ),
+          ],
+        )),
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(255, 58, 45, 50)),
+            onPressed: () {},
+            child: Text('Start Session')),
+      ],
+    );
   }
 }
+
+Widget buildTimeCard({required String time, required String header}) =>
+    Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(
+        time,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+          fontSize: 72,
+        ),
+      ),
+    );
